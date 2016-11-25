11e795579ae35692d3875e77f123322ad44d7cfb
Yudi Xiao <463708580@qq.com>
2016-11-25 16:18:27 +0800

    Main UI Added

---
+++ Talkie-Walkie/Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    public void ToSceneWithLoading(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
